81a22230709561850d2d0f67c6345bcac3ec8262
wawan <wadewawan375@gmail.com>
2024-12-02 15:34:27 +0700

    update requirements

--- backend/django_backend/user_service/user_service/settings.py
+++ backend/django_backend/user_service/user_service/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'user_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jajancuy_db',
+        'USER': 'ade',
+        'PASSWORD': 'your_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
