fe872da441a6c157a7a039dd4b301d827bdb6caf
wadewawan375 <wadewawan375@student.uns.ac.id>
2024-12-24 10:49:17 +0000

    halo from vps

--- backend/backend-python/django_backend/user_services/user_services/settings.py
+++ backend/backend-python/django_backend/user_services/user_services/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_APP')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
